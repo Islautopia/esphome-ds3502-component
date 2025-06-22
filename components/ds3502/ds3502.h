750ce5c237a6ce4fcafd6e8cc31eddc0f8db4f4e
Iñaki <inaki@islautopia.com>
2025-06-22 22:03:59 +0200

    Update ds3502.h

--- components/ds3502/ds3502.h
+++ components/ds3502/ds3502.h
@@ -6,11 +6,22 @@
 namespace esphome {
 namespace ds3502 {
 
-class DS3502Output : public output::FloatOutput, public Component, public i2c::I2CDevice {
+// Clase para el "Hub" I2C
+class DS3502Hub : public Component, public i2c::I2CDevice {
  public:
   void setup() override;
   void dump_config() override;
+  void set_wiper(uint8_t value);
+};
+
+// Clase para el "Output" que usa el Hub
+class DS3502Output : public output::FloatOutput, public Component {
+ public:
+  DS3502Output(DS3502Hub *hub) : hub_(hub) {}
   void write_state(float state) override;
+
+ protected:
+  DS3502Hub *hub_;
 };
 
 }  // namespace ds3502
