040e0847d63b904c64fc29d9f38779337b870b2d
Iñaki <inaki@islautopia.com>
2025-06-22 22:44:52 +0200

    Create ds3502.cpp

---
+++ components/ds3502/output/ds3502.cpp
@@ -0,0 +1,32 @@
+#include "ds3502.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace ds3502 {
+
+static const char *const TAG = "ds3502.output";
+
+void DS3502Output::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up DS3502 Output component...");
+  this->dump_config();
+}
+
+void DS3502Output::dump_config() {
+  LOG_OUTPUT(this);
+  LOG_I2C_DEVICE(this);
+  if (this->is_failed()) {
+    LOG_ERROR(TAG, "Communication with DS3502 failed!");
+  }
+}
+
+void DS3502Output::write_state(float state) {
+  uint8_t wiper_value = static_cast<uint8_t>(state * 127.0f);
+  if (this->write(&wiper_value, 1) != i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "Failed to write to DS3502");
+    return;
+  }
+  ESP_LOGD(TAG, "Setting wiper to %d (state: %.2f)", wiper_value, state);
+}
+
+}  // namespace ds3502
+}  // namespace esphome
