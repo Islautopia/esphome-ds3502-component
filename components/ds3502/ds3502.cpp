f436d300f6e4a9183c9c1f627286212181af6808
Iñaki <inaki@islautopia.com>
2025-06-22 23:02:48 +0200

    Create ds3502.cpp

---
+++ components/ds3502/ds3502.cpp
@@ -0,0 +1,25 @@
+#include "ds3502.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace ds3502 {
+
+static const char *const TAG = "ds3502.hub";
+
+void DS3502Hub::setup() { ESP_LOGCONFIG(TAG, "Setting up DS3502 Hub..."); }
+void DS3502Hub::dump_config() {
+  LOG_I2C_DEVICE(this);
+  if (this->is_failed()) {
+    LOG_ERROR(TAG, "DS3502 Hub communication failed!");
+  }
+}
+void DS3502Hub::set_wiper(uint8_t value) {
+  if (this->write(&value, 1) != i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "Failed to write to DS3502");
+    return;
+  }
+  ESP_LOGD(TAG, "Hub set wiper to %d", value);
+}
+
+}  // namespace ds3502
+}  // namespace esphome
